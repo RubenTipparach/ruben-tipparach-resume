21aacb18d5d3267319eb6cd6e18d405c9f3f670a
DESKTOP-2GCKC3F\ruben <santipab.stephane@gmail.com>
2021-01-30 13:36:11 -0600

    a bunch of resumes for fun

--- generate-all.bat
+++ generate-all.bat
@@ -1,3 +1,16 @@
+call npm install jsonresume-theme-elegant -g
+call npm install jsonresume-theme-paper -g
+call npm install jsonresume-theme-kendall -g
+call npm install jsonresume-theme-flat -g
+call npm install jsonresume-theme-classy -g
+call npm install jsonresume-theme-class -g
+call npm install jsonresume-theme-short -g
+call npm install jsonresume-theme-slick -g
+call npm install jsonresume-theme-kwan -g
+call npm install jsonresume-theme-onepage -g
+call npm install jsonresume-theme-spartan -g
+call npm install jsonresume-theme-stackoverflow -g
+
 call resume export resume-elegant.html --theme elegant
 call resume export resume-paper.html --theme paper
 call resume export resume-kendall.html --theme kendall
@@ -11,3 +24,18 @@ call resume export resume-kwan.html --theme kwan
 call resume export resume-onepage.html --theme onepage
 call resume export resume-spartan.html --theme spartan
 call resume export resume-stackoverflow.html --theme stackoverflow
+
+
+call resume export resume-elegant.pdf --theme elegant
+call resume export resume-paper.pdf --theme paper
+call resume export resume-kendall.pdf --theme kendall
+call resume export resume-flat.pdf --theme flat
+call resume export resume-modern.pdf --theme modern
+call resume export resume-classy.pdf --theme classy
+call resume export resume-class.pdf --theme class
+call resume export resume-short.pdf --theme short
+call resume export resume-slick.pdf --theme slick
+call resume export resume-kwan.pdf --theme kwan
+call resume export resume-onepage.pdf --theme onepage
+call resume export resume-spartan.pdf --theme spartan
+call resume export resume-stackoverflow.pdf --theme stackoverflow
